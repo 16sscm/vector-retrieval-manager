e094308afdbe5817c05250da996948618175b688
yongzhangao <yongzhangao@hiretual.com>
2022-01-12 19:28:34 +0800

    load search engine config for once

--- src/main/java/com/hiretual/vectorretrievalmerge/config/SearchEngineConfig.java
+++ src/main/java/com/hiretual/vectorretrievalmerge/config/SearchEngineConfig.java
@@ -7,14 +7,23 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.hiretual.vectorretrievalmerge.utils.JsonParser;
 import com.hiretual.vectorretrievalmerge.utils.RawJsonReader;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SearchEngineConfig {
     private static String configFile="/root/engine_config.json";
-    public static List<String> getSearchEngines(){
-        List<String>engines=new ArrayList<>();
+    private static List<String>engines=new ArrayList<>();
+    private static final Logger logger=LoggerFactory.getLogger(SearchEngineConfig.class);
+    static {
+        logger.info("Load search engine config from:"+configFile);
         JsonNode jsonNode=JsonParser.getJsonNode(RawJsonReader.readJsonFile(configFile));
         for(JsonNode node :jsonNode){
-            engines.add(node.asText());
+            String engineUrl=node.asText();
+            logger.info("read url:"+engineUrl);
+            engines.add(engineUrl);
         }
+    }
+    public static List<String> getSearchEngines(){
         return engines;
     }
 }
